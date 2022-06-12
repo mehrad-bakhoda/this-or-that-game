a578e1a35f25b58b4da1a6e3e6c3eb9aca3e5da2
BlueShdw <no2.nemesis@gmail.com>
2022-06-12 20:27:28 +0430

    api request fixed and also it can get images from randome categories

--- pages/index.js
+++ pages/index.js
@@ -1,24 +1,43 @@
 import styles from "../styles/Home.module.css";
 import Image from "next/image";
-import superjson from "superjson";
+import { useState } from "react";
 
-export default function Home({ data }) {
-  console.log(data);
-  console.log(data.url);
+export default function Home({ image1, image2, categories }) {
+  const [img1, setImg1] = useState(image1.url);
+  const [img2, setImg2] = useState(image2.url);
+
+  const handleSelect = async () => {
+    let randomElem = categories[Math.floor(Math.random() * categories.length)];
+
+    await fetch(`https://api.waifu.pics/sfw/${randomElem}`, { method: "GET" })
+      .then((response) => response.text())
+      .then((result) => {
+        setImg1(JSON.parse(result).url);
+      })
+      .catch((error) => console.log("error", error));
+    randomElem = categories[Math.floor(Math.random() * categories.length)];
+
+    await fetch(`https://api.waifu.pics/sfw/${randomElem}`, { method: "GET" })
+      .then((response) => response.text())
+      .then((result) => {
+        setImg2(JSON.parse(result).url);
+      })
+      .catch((error) => console.log("error", error));
+  };
   return (
     <div className={styles.main}>
-      <div className={styles.card}>
+      <div className={styles.card} onClick={handleSelect}>
         <Image
-          src={`${data.url}`}
-          alt="Picture of the author"
+          src={`${img1}`}
+          alt="Picture of an anime"
           width={500}
           height={500}
         />
       </div>
-      <div className={styles.card}>
+      <div className={styles.card} onClick={handleSelect}>
         <Image
-          src="/2.jpeg"
-          alt="Picture of the author"
+          src={`${img2}`}
+          alt="Picture of an anime"
           width={500}
           height={500}
         />
@@ -27,18 +46,63 @@ export default function Home({ data }) {
   );
 }
 export async function getServerSideProps() {
-  let data = {};
+  let image1 = {};
+  let image2 = {};
+  const categories = [
+    "waifu",
+    "neko",
+    "shinobu",
+    "megumin",
+    "bully",
+    "cuddle",
+    "cry",
+    "hug",
+    "awoo",
+    "kiss",
+    "lick",
+    "pat",
+    "smug",
+    "bonk",
+    "yeet",
+    "blush",
+    "smile",
+    "wave",
+    "highfive",
+    "handhold",
+    "mom",
+    "bite",
+    "glomp",
+    "slap",
+    "kill",
+    "kick",
+    "happy",
+    "wink",
+    "poke",
+    "dance",
+    "cringe",
+  ];
+  let randomElem = categories[Math.floor(Math.random() * categories.length)];
 
-  await fetch("https://api.waifu.pics/sfw/bonk", { method: "GET" })
+  await fetch(`https://api.waifu.pics/sfw/${randomElem}`, { method: "GET" })
     .then((response) => response.text())
     .then((result) => {
-      data = result;
+      image1 = JSON.parse(result);
+    })
+    .catch((error) => console.log("error", error));
+  randomElem = categories[Math.floor(Math.random() * categories.length)];
+
+  await fetch(`https://api.waifu.pics/sfw/${randomElem}`, { method: "GET" })
+    .then((response) => response.text())
+    .then((result) => {
+      image2 = JSON.parse(result);
     })
     .catch((error) => console.log("error", error));
 
   return {
     props: {
-      data,
+      image1,
+      image2,
+      categories,
     },
   };
 }
